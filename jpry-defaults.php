6c69030da73529b84e933c9ebc67f19bd5c695be
Jeremy Pry <JPry@users.noreply.github.com>
2014-08-26 13:55:25 -0400

    Create jpry-defaults.php

---
+++ jpry-defaults.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: JPry Defaults
+ * Plugin URI: https://github.com/PrysPlugins/jpry-defaults
+ * Description: Default settings for new JPry sites.
+ * Version: 1.0
+ * Author: Jeremy Pry
+ * Author URI: http://jeremypry.com/
+ * License: GPL2
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+	die( "You can't do anything by accessing this file directly." );
+}
+
+
